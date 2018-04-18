fbe2bcf542ca67bc2fce2625ca0afb2d212b5da5
Emad Jacob Maroun <emoun.open@gmail.com>
2018-04-18 13:38:41 +0200

    Modified AstVisitor to match the use of Rc in the AST.

--- src/micro_c/ast_visitor.rs
+++ src/micro_c/ast_visitor.rs
@@ -5,82 +5,83 @@ use micro_c::{
 	Expression, Expression::*,
 	Type, BinaryOperator, UnaryOperator
 };
+use std::rc::Rc;
 
 pub trait AstVisitor{
 //Enter methods
-	fn enter_block(&mut self, block: &Block);
-	fn enter_declaration(&mut self, decl: &Declaration);
+	fn enter_block(&mut self, block: Rc<Block>);
+	fn enter_declaration(&mut self, decl: Rc<Declaration>);
 	fn enter_declaration_variable(&mut self, t:Type, name: &str){}
 	fn enter_declaration_array(&mut self, t:Type, name: &str, len: i32){}
-	fn enter_declaration_composite(&mut self, c1: &Declaration, c2: &Declaration){}
+	fn enter_declaration_composite(&mut self, c1: Rc<Declaration>, c2: Rc<Declaration>){}
 	
-	fn enter_statement(&mut self, stmt: &Statement);
-	fn enter_statement_assign(&mut self, name: &str, expr: &Expression){}
-	fn enter_statement_assign_array(&mut self, name: &str, index: &Expression, expr: &Expression){}
+	fn enter_statement(&mut self, stmt: Rc<Statement>);
+	fn enter_statement_assign(&mut self, name: &str, expr: Rc<Expression>){}
+	fn enter_statement_assign_array(&mut self, name: &str, index: Rc<Expression>, expr: Rc<Expression>){}
 	fn enter_statement_if_else(&mut self,
-							   cond: &Expression, if_true: &Block, if_false: Option<&Block>){}
-	fn enter_statement_while(&mut self, cond: &Expression, body: &Block){}
+							   cond: Rc<Expression>, if_true: Rc<Block>, if_false: Option<Rc<Block>>){}
+	fn enter_statement_while(&mut self, cond: Rc<Expression>, body: Rc<Block>){}
 	fn enter_statement_read(&mut self, var: &str){}
-	fn enter_statement_read_array(&mut self, arr: &str, index: &Expression){}
-	fn enter_statement_write(&mut self, value: &Expression){}
+	fn enter_statement_read_array(&mut self, arr: &str, index: Rc<Expression>){}
+	fn enter_statement_write(&mut self, value: Rc<Expression>){}
 	fn enter_statement_break(&mut self){}
 	fn enter_statement_continue(&mut self){}
-	fn enter_statement_composite(&mut self, s1: &Statement, s2:&Statement){}
-	fn enter_statement_scope(&mut self, block: &Block){}
+	fn enter_statement_composite(&mut self, s1: Rc<Statement>, s2:Rc<Statement>){}
+	fn enter_statement_scope(&mut self, block: Rc<Block>){}
 	
-	fn enter_expression(&mut self, expr: &Expression);
+	fn enter_expression(&mut self, expr: Rc<Expression>);
 	fn enter_expression_constant(&mut self, value: i32){}
 	fn enter_expression_variable(&mut self, name: &str){}
-	fn enter_expression_array_access(&mut self, name: &str, index: &Expression){}
-	fn enter_expression_binary(&mut self, lhs: &Expression, op: BinaryOperator, rhs: &Expression){}
-	fn enter_expression_unary(&mut self, op: UnaryOperator, rhs: &Expression){}
+	fn enter_expression_array_access(&mut self, name: &str, index: Rc<Expression>){}
+	fn enter_expression_binary(&mut self, lhs: Rc<Expression>, op: BinaryOperator, rhs: Rc<Expression>){}
+	fn enter_expression_unary(&mut self, op: UnaryOperator, rhs: Rc<Expression>){}
 	
 //exit methods
-	fn exit_block(&mut self, block: &Block);
-	fn exit_declaration(&mut self, decl: &Declaration);
+	fn exit_block(&mut self, block: Rc<Block>);
+	fn exit_declaration(&mut self, decl: Rc<Declaration>);
 	fn exit_declaration_variable(&mut self, t:Type, name: &str){}
 	fn exit_declaration_array(&mut self, t:Type, name: &str, len: i32){}
-	fn exit_declaration_composite(&mut self, c1: &Declaration, c2: &Declaration){}
+	fn exit_declaration_composite(&mut self, c1: Rc<Declaration>, c2: Rc<Declaration>){}
 	
-	fn exit_statement(&mut self, stmt: &Statement);
-	fn exit_statement_assign(&mut self, name: &str, expr: &Expression){}
-	fn exit_statement_assign_array(&mut self, name: &str, index: &Expression, expr: &Expression){}
+	fn exit_statement(&mut self, stmt: Rc<Statement>);
+	fn exit_statement_assign(&mut self, name: &str, expr: Rc<Expression>){}
+	fn exit_statement_assign_array(&mut self, name: &str, index: Rc<Expression>, expr: Rc<Expression>){}
 	fn exit_statement_if_else(&mut self,
-							   cond: &Expression, if_true: &Block, if_false: Option<&Block>){}
-	fn exit_statement_while(&mut self, cond: &Expression, body: &Block){}
+							   cond: Rc<Expression>, if_true: Rc<Block>, if_false: Option<Rc<Block>>){}
+	fn exit_statement_while(&mut self, cond: Rc<Expression>, body: Rc<Block>){}
 	fn exit_statement_read(&mut self, var: &str){}
-	fn exit_statement_read_array(&mut self, arr: &str, index: &Expression){}
-	fn exit_statement_write(&mut self, value: &Expression){}
+	fn exit_statement_read_array(&mut self, arr: &str, index: Rc<Expression>){}
+	fn exit_statement_write(&mut self, value: Rc<Expression>){}
 	fn exit_statement_break(&mut self){}
 	fn exit_statement_continue(&mut self){}
-	fn exit_statement_composite(&mut self, s1: &Statement, s2:&Statement){}
-	fn exit_statement_scope(&mut self, block: &Block){}
+	fn exit_statement_composite(&mut self, s1: Rc<Statement>, s2:Rc<Statement>){}
+	fn exit_statement_scope(&mut self, block: Rc<Block>){}
 	
-	fn exit_expression(&mut self, expr: &Expression);
+	fn exit_expression(&mut self, expr: Rc<Expression>);
 	fn exit_expression_constant(&mut self, value: i32){}
 	fn exit_expression_variable(&mut self, name: &str){}
-	fn exit_expression_array_access(&mut self, name: &str, index: &Expression){}
-	fn exit_expression_binary(&mut self, lhs: &Expression, op: BinaryOperator, rhs: &Expression){}
-	fn exit_expression_unary(&mut self, op: UnaryOperator, rhs: &Expression){}
+	fn exit_expression_array_access(&mut self, name: &str, index: Rc<Expression>){}
+	fn exit_expression_binary(&mut self, lhs: Rc<Expression>, op: BinaryOperator, rhs: Rc<Expression>){}
+	fn exit_expression_unary(&mut self, op: UnaryOperator, rhs: Rc<Expression>){}
 	
 //Visit methods
-	fn visit(&mut self, ast: &Block){
+	fn visit(&mut self, ast: Rc<Block>){
 		self.visit_block(ast);
 	}
 
-	fn visit_block(&mut self, block: &Block){
-		self.enter_block(block);
+	fn visit_block(&mut self, block: Rc<Block>){
+		self.enter_block(block.clone());
 		if let Some(ref decl) = block.declarations {
-			self.visit_declaration(decl);
+			self.visit_declaration(decl.clone());
 		}
-		self.visit_statement(block.statements.as_ref());
+		self.visit_statement(block.statements.clone());
 		self.exit_block(block);
 	}
-	fn visit_declaration(&mut self, decl: &Declaration){
-		self.enter_declaration(decl);
-		if let &Declaration::Composite(ref d1,ref d2) = decl {
-			self.visit_declaration(d1);
-			self.visit_declaration(d2);
+	fn visit_declaration(&mut self, decl: Rc<Declaration>){
+		self.enter_declaration(decl.clone());
+		if let Declaration::Composite(ref d1,ref d2) = *decl {
+			self.visit_declaration(d1.clone());
+			self.visit_declaration(d2.clone());
 		}
 		self.exit_declaration(decl);
 	}
@@ -92,76 +93,76 @@ pub trait AstVisitor{
 		self.enter_declaration_array(t, name, len);
 		self.exit_declaration_array(t, name, len);
 	}
-	fn visit_declaration_composite(&mut self, c1: &Declaration, c2: &Declaration){
-		self.enter_declaration_composite(c1, c2);
-		self.visit_declaration(c1);
-		self.visit_declaration(c2);
+	fn visit_declaration_composite(&mut self, c1: Rc<Declaration>, c2: Rc<Declaration>){
+		self.enter_declaration_composite(c1.clone(), c2.clone());
+		self.visit_declaration(c1.clone());
+		self.visit_declaration(c2.clone());
 		self.exit_declaration_composite(c1, c2);
 	}
 	
-	fn visit_statement(&mut self, stmt: &Statement){
-		self.enter_statement(stmt);
+	fn visit_statement(&mut self, stmt: Rc<Statement>){
+		self.enter_statement(stmt.clone());
 		
-		match stmt {
-			&Assign(name,ref expr) =>self.visit_statement_assign(name, expr),
-			&AssignArray(name,ref index,ref expr) =>self.visit_statement_assign_array(name, index, expr),
-			&IfElse(ref cond, ref if_true, ref if_false) =>
-				if let &Some(ref block) = if_false {
-					self.visit_statement_if_else(cond, if_true, Some(block))
+		match *stmt {
+			Assign(ref name, ref expr) =>self.visit_statement_assign(name, expr.clone()),
+			AssignArray(name, ref index, ref expr) =>self.visit_statement_assign_array(name, index.clone(), expr.clone()),
+			IfElse( ref cond,  ref if_true,  ref if_false) =>
+				if let Some(ref block) = *if_false {
+					self.visit_statement_if_else(cond.clone(), if_true.clone(), Some(block.clone()))
 				} else {
-					self.visit_statement_if_else(cond, if_true, None)
+					self.visit_statement_if_else(cond.clone(), if_true.clone(), None)
 				},
-			&While(ref cond, ref body) => self.visit_statement_while(cond, body),
-			&Read(var) => self.visit_statement_read(var),
-			&ReadArray(arr, ref index) => self.visit_statement_read_array(arr, index),
-			&Write(ref expr) => self.visit_statement_write(expr),
-			&Break => self.visit_statement_break(),
-			&Continue => self.visit_statement_continue(),
-			&Composite(ref s1, ref s2) => self.visit_statement_composite(s1, s2),
-			&Scope(ref block) => self.visit_statement_scope(block)
+			While(ref cond, ref body) => self.visit_statement_while(cond.clone(), body.clone()),
+			Read(ref var) => self.visit_statement_read(var),
+			ReadArray(arr,ref index) => self.visit_statement_read_array(arr, index.clone()),
+			Write(ref expr) => self.visit_statement_write(expr.clone()),
+			Break => self.visit_statement_break(),
+			Continue => self.visit_statement_continue(),
+			Composite(ref s1, ref s2) => self.visit_statement_composite(s1.clone(), s2.clone()),
+			Scope(ref block) => self.visit_statement_scope(block.clone())
 		}
 		
 		self.exit_statement(stmt);
 	}
-	fn visit_statement_assign(&mut self, name: &str, expr: &Expression){
-		self.enter_statement_assign(name, expr);
-		self.visit_expression(expr);
+	fn visit_statement_assign(&mut self, name: &str, expr: Rc<Expression>){
+		self.enter_statement_assign(name, expr.clone());
+		self.visit_expression(expr.clone());
 		self.exit_statement_assign(name, expr);
 	}
-	fn visit_statement_assign_array(&mut self, name: &str, index: &Expression, expr: &Expression){
-		self.enter_statement_assign_array(name, index, expr);
-		self.visit_expression(index);
-		self.visit_expression(expr);
-		self.exit_statement_assign_array(name, index, expr);
+	fn visit_statement_assign_array(&mut self, name: &str, index: Rc<Expression>, expr: Rc<Expression>){
+		self.enter_statement_assign_array(name, index.clone(), expr.clone());
+		self.visit_expression(index.clone());
+		self.visit_expression(expr.clone());
+		self.exit_statement_assign_array(name, index.clone(), expr.clone());
 	}
 	fn visit_statement_if_else(&mut self,
-							   cond: &Expression, if_true: &Block, if_false: Option<&Block>){
-		self.enter_statement_if_else(cond, if_true, if_false);
-		self.visit_expression(cond);
-		self.visit_block(if_true);
-		if let Some(block) = if_false {
-			self.visit_block(block);
+							   cond: Rc<Expression>, if_true: Rc<Block>, if_false: Option<Rc<Block>>){
+		self.enter_statement_if_else(cond.clone(), if_true.clone(), if_false.clone());
+		self.visit_expression(cond.clone());
+		self.visit_block(if_true.clone());
+		if let Some(ref block) = if_false {
+			self.visit_block(block.clone());
 		}
 		self.exit_statement_if_else(cond, if_true, if_false);
 	}
-	fn visit_statement_while(&mut self, cond: &Expression, body: &Block){
-		self.enter_statement_while(cond, body);
-		self.visit_expression(cond);
-		self.visit_block(body);
+	fn visit_statement_while(&mut self, cond: Rc<Expression>, body: Rc<Block>){
+		self.enter_statement_while(cond.clone(), body.clone());
+		self.visit_expression(cond.clone());
+		self.visit_block(body.clone());
 		self.exit_statement_while(cond, body);
 	}
 	fn visit_statement_read(&mut self, var: &str){
 		self.enter_statement_read(var);
 		self.exit_statement_read(var);
 	}
-	fn visit_statement_read_array(&mut self, arr: &str, index: &Expression){
-		self.enter_statement_read_array(arr, index);
-		self.visit_expression(index);
+	fn visit_statement_read_array(&mut self, arr: &str, index: Rc<Expression>){
+		self.enter_statement_read_array(arr, index.clone());
+		self.visit_expression(index.clone());
 		self.exit_statement_read_array(arr, index);
 	}
-	fn visit_statement_write(&mut self, value: &Expression){
-		self.enter_statement_write(value);
-		self.visit_expression(value);
+	fn visit_statement_write(&mut self, value: Rc<Expression>){
+		self.enter_statement_write(value.clone());
+		self.visit_expression(value.clone());
 		self.exit_statement_write(value);
 	}
 	fn visit_statement_break(&mut self){
@@ -172,27 +173,27 @@ pub trait AstVisitor{
 		self.enter_statement_continue();
 		self.exit_statement_continue();
 	}
-	fn visit_statement_composite(&mut self, s1: &Statement, s2:&Statement){
-		self.enter_statement_composite(s1, s2);
-		self.visit_statement(s1);
-		self.visit_statement(s2);
+	fn visit_statement_composite(&mut self, s1: Rc<Statement>, s2:Rc<Statement>){
+		self.enter_statement_composite(s1.clone(), s2.clone());
+		self.visit_statement(s1.clone());
+		self.visit_statement(s2.clone());
 		self.exit_statement_composite(s1, s2);
 	}
-	fn visit_statement_scope(&mut self, block: &Block){
-		self.enter_statement_scope(block);
-		self.visit_block(block);
+	fn visit_statement_scope(&mut self, block: Rc<Block>){
+		self.enter_statement_scope(block.clone());
+		self.visit_block(block.clone());
 		self.exit_statement_scope(block);
 	}
 	
-	fn visit_expression(&mut self, expr: &Expression){
-		self.enter_expression(expr);
+	fn visit_expression(&mut self, expr: Rc<Expression>){
+		self.enter_expression(expr.clone());
 		
-		match expr {
-			&Constant(n) => self.visit_expression_constant(n),
-			&Variable(x) => self.visit_expression_variable(x),
-			&ArrayAccess(x, ref index) => self.visit_expression_array_access(x, index),
-			&Binary(ref lhs, op, ref rhs) => self.visit_expression_binary(lhs, op, rhs),
-			&Unary(op, ref rhs) => self.visit_expression_unary(op, rhs),
+		match *expr {
+			Constant(n) => self.visit_expression_constant(n),
+			Variable(x) => self.visit_expression_variable(x),
+			ArrayAccess(x, ref index) => self.visit_expression_array_access(x, index.clone()),
+			Binary(ref lhs, op, ref rhs) => self.visit_expression_binary(lhs.clone(), op, rhs.clone()),
+			Unary(op, ref rhs) => self.visit_expression_unary(op, rhs.clone()),
 		}
 		
 		self.exit_expression(expr);
@@ -205,20 +206,20 @@ pub trait AstVisitor{
 		self.enter_expression_variable(name);
 		self.exit_expression_variable(name);
 	}
-	fn visit_expression_array_access(&mut self, name: &str, index: &Expression){
-		self.enter_expression_array_access(name, index);
-		self.visit_expression(index);
+	fn visit_expression_array_access(&mut self, name: &str, index: Rc<Expression>){
+		self.enter_expression_array_access(name, index.clone());
+		self.visit_expression(index.clone());
 		self.exit_expression_array_access(name, index);
 	}
-	fn visit_expression_binary(&mut self, lhs: &Expression, op: BinaryOperator, rhs: &Expression){
-		self.enter_expression_binary(lhs, op, rhs);
-		self.visit_expression(lhs);
-		self.visit_expression(rhs);
+	fn visit_expression_binary(&mut self, lhs: Rc<Expression>, op: BinaryOperator, rhs: Rc<Expression>){
+		self.enter_expression_binary(lhs.clone(), op, rhs.clone());
+		self.visit_expression(lhs.clone());
+		self.visit_expression(rhs.clone());
 		self.exit_expression_binary(lhs, op, rhs);
 	}
-	fn visit_expression_unary(&mut self, op: UnaryOperator, rhs: &Expression){
-		self.enter_expression_unary(op, rhs);
-		self.visit_expression(rhs);
+	fn visit_expression_unary(&mut self, op: UnaryOperator, rhs: Rc<Expression>){
+		self.enter_expression_unary(op, rhs.clone());
+		self.visit_expression(rhs.clone());
 		self.exit_expression_unary(op, rhs);
 	}
 }
--- src/micro_c/program_graph/grapher.rs
+++ src/micro_c/program_graph/grapher.rs
@@ -6,6 +6,7 @@ use micro_c::{
 	Expression, Expression::*,
 	Type, BinaryOperator, UnaryOperator
 };
+use std::rc::Rc;
 
 struct ProgramGrapher{
 
@@ -13,34 +14,34 @@ struct ProgramGrapher{
 
 impl AstVisitor for ProgramGrapher{
 	//Enter methods
-	fn enter_block(&mut self, block: &Block){
+	fn enter_block(&mut self, block: Rc<Block>){
 		unimplemented!()
 	}
-	fn enter_declaration(&mut self, decl: &Declaration){
-		unimplemented!()
-	}
-	
-	fn enter_statement(&mut self, stmt: &Statement){
+	fn enter_declaration(&mut self, decl: Rc<Declaration>){
 		unimplemented!()
 	}
 	
-	fn enter_expression(&mut self, expr: &Expression){
+	fn enter_statement(&mut self, stmt: Rc<Statement>){
+		unimplemented!()
+	}
+	
+	fn enter_expression(&mut self, expr: Rc<Expression>){
 		unimplemented!()
 	}
 	
 	//exit methods
-	fn exit_block(&mut self, block: &Block){
+	fn exit_block(&mut self, block: Rc<Block>){
 		unimplemented!()
 	}
-	fn exit_declaration(&mut self, decl: &Declaration){
-		unimplemented!()
-	}
-	
-	fn exit_statement(&mut self, stmt: &Statement){
+	fn exit_declaration(&mut self, decl: Rc<Declaration>){
 		unimplemented!()
 	}
 	
-	fn exit_expression(&mut self, expr: &Expression){
+	fn exit_statement(&mut self, stmt: Rc<Statement>){
+		unimplemented!()
+	}
+	
+	fn exit_expression(&mut self, expr: Rc<Expression>){
 		unimplemented!()
 	}
 	
